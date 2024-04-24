422590b4e759454b61f8a2d1c28af1b3f9621291
M4rrix <114517347+M4rrix@users.noreply.github.com>
2024-04-24 17:43:39 -0300

    funciones

--- 04.Funciones/04-funciones.js
+++ 04.Funciones/04-funciones.js
@@ -5,6 +5,14 @@
 //Ejemplo "Bienvenido Leonardo Avila" -> IMPORTANTE la funcion tiene que llamarse 'bienvenida'
 //Tu código:
 
+function bienvenida(firstname, lastname) {
+    return "Bienvenida " + firstname + "" + lastname;
+
+}
+
+console.log(bienvenida("Maria", "Camaña"));
+
+
 
 
 /**********************************************************************************/
@@ -12,7 +20,37 @@
 // IMPORTANTE -> la funcion debe llamarse 'calc'
 // Tu código:
 
+function calc(num1, operador, num2) {
+   let resultado;
 
+   switch (operador) {
+    case '+':
+        resultado = num1 + num2;
+        break;
+    case '-':
+        resultado = num1 - num2;
+        break;
+    case '*':
+        resultado = num1 * num2;
+        break;
+    case '/':
+        if (num2 !== 0) {
+        resultado = num1 / num2;
+        }else {
+            resultado = "Error: Division por cero"
+        }
+        break;
+    default:
+        resultado = "Operador no valido"
+}
+return resultado;
+
+}
+
+console.log(calc(7, '+', 4));
+console.log(calc(7, '-', 4));
+console.log(calc(7, '*', 4));
+console.log(calc(7, '/', 4));
 
 /**********************************************************************************/
 // Crea una funcion que en base a la temperaturma maxima, min y porcentaje de lluvia, diga si es necesario salir con abrigo,
@@ -25,6 +63,27 @@
 // IMPORTANTE -> la funcion debe llamarse 'temp'    
 // Tu código:
 
+function temp = (max, min, porcentajeLluvia) => {
+    let mensaje = "";
+
+    if(max < 15) {
+        mensaje = "Es recomdable salir con abrigo";
+    } else if (max = 26 && min > 17) {
+        mensaje = "El dia esta lindo";
+    } else if (min > 25) {
+        mensaje = "El dia esta caluroso";
+    } 
+
+    if (porcentajeLluvia > 50) {
+        mensaje += ", recomendable un paraguas";
+        
+    }
+    return mensaje;
+
+}
+ 
+console.log(temp(10, 5, 60));
+console.log(temp(11, 3, 9));
 
 
 /**********************************************************************************/
@@ -35,12 +94,35 @@
 // Tu código:
 
 
+    function checkPassword(contrasena) {
+        if(contrasena.length >= 8) {
+            return "La contrasena es segura"
+        } else {
+            return "la contrasena es insegura"
+        }
 
+    }
+
+console.log(checkPassword("abdkbv12470"))
 
 /**********************************************************************************/
 // Crea una función que reciba un string como parámetro y devuelva el mismo string pero con todas las palabras en orden inverso.
 // IMPORTANTE -> la funcion debe llamarse 'stringInverso'
 // Tu código:
+
+function stringInverso (frase) {
+       // Dividir el string en un array de palabras
+    let palabras = str.split(" ");
+    // Invertir el orden de las palabras en el array
+    let palabrasInversas = palabras.reverse();
+    // Unir las palabras invertidas en un nuevo string
+    let resultado = palabrasInversas.join(" ");
+    // Devolver el resultado
+    return resultado;
+}
+
+// Ejemplo de uso
+console.log(stringInverso("Hola mundo")); // Salida: "mundo Hola"
 
 
 
